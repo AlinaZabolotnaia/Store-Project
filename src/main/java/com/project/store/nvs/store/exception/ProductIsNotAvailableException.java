11fbb46e5bad9e52d923af77ea69143558ffa827
Ivan <trembaci1993@gmail.com>
2025-10-17 23:40:24 +0300

    product feature

---
+++ src/main/java/com/project/store/nvs/store/exception/ProductIsNotAvailableException.java
@@ -0,0 +1,12 @@
+package com.project.store.nvs.store.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ProductIsNotAvailableException extends RuntimeException{
+
+    public ProductIsNotAvailableException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/project/store/nvs/store/models/product/Product.java
@@ -0,0 +1,27 @@
+package com.project.store.nvs.store.models.product;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor(force = true)
+@AllArgsConstructor
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NonNull
+    private Long id;
+    @NonNull
+    private String title;
+    @NonNull
+    private Integer available;
+    @NonNull
+    private BigDecimal price;
+
+}
